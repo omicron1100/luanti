v-rob/minetest

Create UI event handling infrastructure

src/ui/window.cpp
@@ -14,6 +14,8 @@
 #include "ui/static_elems.h"
 #include "util/serialize.h"
 
+#include <SDL2/SDL.h>
+
 namespace ui
 {
 	SizeI getTextureSize(video::ITexture *texture)
@@ -50,6 +52,30 @@ namespace ui
 		return nullptr;
 	}
 
+	Elem *Window::getNextElem(Elem *elem, bool reverse)
+	{
+		size_t next = elem->getOrder();
+		size_t last = m_ordered_elems.size() - 1;
+
+		if (!reverse) {
+			next = (next == last) ? 0 : next + 1;
+		} else {
+			next = (next == 0) ? last : next - 1;
+		}
+
+		return m_ordered_elems[next];
+	}
+
+	void Window::clearElem(Elem *elem)
+	{
+		if (m_focused_elem == elem) {
+			m_focused_elem = nullptr;
+		}
+		if (m_hovered_elem == elem) {
+			m_hovered_elem = nullptr;
+		}
+	}
+
 	const std::string *Window::getStyleStr(u32 index) const
 	{
 		if (index < m_style_strs.size()) {
@@ -66,10 +92,18 @@ namespace ui
 		m_root_elem = nullptr;
 
 		m_style_strs.clear();
+
+		m_focused_elem = nullptr;
+		m_hovered_elem = nullptr;
+
+		m_uncloseable = false;
+		m_events = 0;
 	}
 
 	bool Window::read(std::istream &is, bool opening)
 	{
+		// Read in all the fundamental properties that must be unconditionally
+		// provided for the window.
 		std::unordered_map<Elem *, std::string> elem_contents;
 		readElems(is, elem_contents);
 
@@ -82,8 +116,26 @@ namespace ui
 		if (opening)
 			m_type = toWindowType(readU8(is));
 
-		// Finally, we can proceed to read in all the element properties.
-		return updateElems(elem_contents);
+		// After the unconditional properties, read the conditional ones.
+		u32 set_mask = readU32(is);
+
+		bool set_focus = false;
+		Elem *new_focused = nullptr;
+
+		if (testShift(set_mask)) {
+			new_focused = getElem(readNullStr(is), false);
+			set_focus = true;
+		}
+
+		if (opening)
+			m_uncloseable = testShift(set_mask);
+
+		if (testShift(set_mask))
+			enableEvent(ON_SUBMIT);
+		if (testShift(set_mask))
+			enableEvent(ON_FOCUS_CHANGE);
+
+		return updateElems(elem_contents, set_focus, new_focused);
 	}
 
 	float Window::getScale() const
@@ -95,6 +147,14 @@ namespace ui
 	{
 		SizeF size = RenderingEngine::get_video_driver()->getCurrentRenderTargetSize();
 		return size / getScale();
+	}
+
+	PosF Window::getPointerPos() const
+	{
+		int x, y;
+		SDL_GetMouseState(&x, &y);
+
+		return PosF(x, y) / getScale();
 	}
 
 	void Window::drawRect(RectF dst, RectF clip, video::SColor color)
@@ -141,8 +201,334 @@ namespace ui
 		RectF parent_rect(getScreenSize());
 		m_root_elem->relayoutAll(parent_rect, parent_rect);
 
+		// Find the current hovered element, which will be nothing if we're not
+		// focused. This may have changed between draw calls due to window
+		// resizes or element layouting.
+		updateHoveredElem();
+
+		// If this window isn't focused, tell the currently focused element.
+		if (!isFocused()) {
+			SDL_Event notice = createUiEvent(UI_FOCUS_SUBVERTED);
+			sendTreeInput(m_focused_elem, notice, true);
+		}
+
 		// Draw all of the newly layouted and updated elements.
 		m_root_elem->drawAll();
+	}
+
+	bool Window::isFocused() const
+	{
+		return g_manager.isFocused() && g_manager.getFocused() == this;
+	}
+
+	bool Window::processInput(const SDL_Event &event)
+	{
+		switch (event.type) {
+		case SDL_KEYDOWN:
+		case SDL_KEYUP: {
+			// Send the keypresses off to the focused element for processing.
+			// The hovered element never gets keypresses.
+			if (sendFocusedInput(event) != nullptr) {
+				return true;
+			}
+
+			if (event.type == SDL_KEYDOWN) {
+				u16 mod = event.key.keysym.mod;
+
+				switch (event.key.keysym.sym) {
+				case SDLK_ESCAPE:
+					// If we got an escape keypress, close the window.
+					close();
+					return true;
+
+				case SDLK_RETURN:
+				case SDLK_RETURN2:
+				case SDLK_KP_ENTER:
+					// If the enter key was pressed but not handled by any
+					// elements, send a submit event to the server.
+					if (testEvent(ON_SUBMIT)) {
+						g_manager.sendMessage(createEvent(ON_SUBMIT).str());
+					}
+					return true;
+
+				case SDLK_TAB:
+				case SDLK_KP_TAB:
+					// If we got a tab key press, but not a ctrl-tab (which is
+					// reserved for use by elements), focus the next element,
+					// or the previous element if shift is pressed.
+					if (!(mod & KMOD_CTRL)) {
+						focusNextElem(mod & KMOD_SHIFT);
+						return true;
+					}
+					break;
+
+				default:
+					break;
+				}
+			}
+
+			return false;
+		}
+
+		case SDL_MOUSEMOTION:
+		case SDL_MOUSEBUTTONDOWN:
+		case SDL_MOUSEBUTTONUP:
+		case SDL_MOUSEWHEEL: {
+			// Make sure that we have an updated hovered element so that the
+			// hovered element is the one that receives the mouse motion event.
+			if (event.type == SDL_MOUSEMOTION) {
+				updateHoveredElem();
+			}
+
+			// If we just clicked with the left mouse button, see if there's
+			// any element at that position to focus.
+			if (event.type == SDL_MOUSEBUTTONDOWN &&
+					event.button.button == SDL_BUTTON_LEFT) {
+				if (isPointerOutside()) {
+					changeFocusedElem(nullptr, true);
+				} else {
+					updateFocusedElem();
+				}
+			}
+
+			// First, give the focused element a chance to see the mouse event,
+			// so it can e.g. unpress a button if a mouse button was released.
+			if (sendFocusedInput(event) != nullptr) {
+				return true;
+			}
+
+			// Then, send the mouse input to the hovered element.
+			if (sendHoveredInput(event) != nullptr) {
+				return true;
+			}
+
+			// If we double clicked outside any element, then close the window.
+			if (event.type == SDL_MOUSEBUTTONDOWN &&
+					event.button.button == SDL_BUTTON_LEFT &&
+					event.button.clicks >= 2 && isPointerOutside()) {
+				close();
+				return true;
+			}
+
+			return false;
+		}
+
+		default:
+			return false;
+		}
+	}
+
+	void Window::enableEvent(u32 event)
+	{
+		m_events |= (1 << event);
+	}
+
+	bool Window::testEvent(u32 event) const
+	{
+		return m_events & (1 << event);
+	}
+
+	std::ostringstream Window::createEvent(u32 event) const
+	{
+		auto os = newOs();
+
+		writeU8(os, Manager::WINDOW_EVENT);
+		writeU64(os, m_id);
+		writeU8(os, event);
+
+		return os;
+	}
+
+	void Window::close()
+	{
+		if (!m_uncloseable) {
+			// Always send the close event so the server can update its
+			// internal window tables, even if there's no on_close() handler.
+			g_manager.sendMessage(createEvent(ON_CLOSE).str());
+
+			// This causes the window object to be destroyed. Do not run any
+			// code after this!
+			g_manager.removeWindow(m_id);
+		}
+	}
+
+	Elem *Window::sendTreeInput(Elem *elem, const SDL_Event &event, bool direct)
+	{
+		// Give the event to the element and all its parents for processing.
+		while (elem != nullptr) {
+			bool handled = elem->processInput(event);
+
+			if (handled) {
+				// If we handled the event, return the element that handled it.
+				return elem;
+			} else if (direct) {
+				// If this event is only intended directly for this element and
+				// it didn't handle the event, then we're done.
+				return nullptr;
+			}
+
+			// Otherwise, give the parent a chance to handle it.
+			elem = elem->getParent();
+		}
+		return nullptr;
+	}
+
+	Elem *Window::sendHoveredInput(const SDL_Event &event)
+	{
+		// We want to get the topmost hovered element, so we have to iterate in
+		// reverse draw order and check each element the mouse is inside.
+		for (size_t i = m_ordered_elems.size(); i > 0; i--) {
+			Elem *elem = m_ordered_elems[i - 1];
+			if (elem->hasPointedBox() && elem->processInput(event)) {
+				return elem;
+			}
+		}
+
+		return nullptr;
+	}
+
+	Elem *Window::sendFocusedInput(const SDL_Event &event)
+	{
+		if (m_focused_elem == nullptr) {
+			return nullptr;
+		}
+
+		// Send the event to the focused element and its parents.
+		Elem *handled = sendTreeInput(m_focused_elem, event, false);
+
+		// If one of the focused element's parents handled the event, let the
+		// focused element know that focus was subverted.
+		if (handled != nullptr && handled != m_focused_elem) {
+			SDL_Event notice = createUiEvent(UI_FOCUS_SUBVERTED);
+			sendTreeInput(m_focused_elem, notice, true);
+		}
+
+		return handled;
+	}
+
+	void Window::changeFocusedElem(Elem *new_focused, bool send_event)
+	{
+		// If the same element is being focused, do nothing.
+		if (new_focused == m_focused_elem) {
+			return;
+		}
+
+		Elem *old_focused = m_focused_elem;
+		m_focused_elem = new_focused;
+
+		// Let the old and new focused elements know that things have
+		// changed, and their parent elements too.
+		SDL_Event notice = createUiEvent(UI_FOCUS_CHANGED, old_focused, new_focused);
+
+		sendTreeInput(old_focused, notice, false);
+		sendTreeInput(new_focused, notice, false);
+
+		// If the server wants to know when focus changes, send it an event.
+		if (send_event && testEvent(ON_FOCUS_CHANGE)) {
+			auto os = createEvent(ON_FOCUS_CHANGE);
+
+			// If either the old or the new element was unfocused, send an
+			// empty string. Otherwise, send the ID of the element.
+			writeNullStr(os, old_focused == nullptr ? "" : old_focused->getId());
+			writeNullStr(os, new_focused == nullptr ? "" : new_focused->getId());
+
+			g_manager.sendMessage(os.str());
+		}
+	}
+
+	bool Window::requestFocusedElem(Elem *new_focused, bool send_event)
+	{
+		// If this element's already focused, we don't need to do anything.
+		if (new_focused == m_focused_elem) {
+			return m_focused_elem;
+		}
+
+		SDL_Event notice = createUiEvent(UI_FOCUS_REQUEST);
+
+		// Ask the new element if it can take user focus. If it can, make it
+		// the focused element.
+		if (sendTreeInput(new_focused, notice, true) == new_focused) {
+			changeFocusedElem(new_focused, send_event);
+			return true;
+		}
+		return false;
+	}
+
+	void Window::focusNextElem(bool reverse)
+	{
+		// Start tabbing from the focused element if there is one, or the root
+		// element otherwise.
+		Elem *start = m_focused_elem != nullptr ? m_focused_elem : m_root_elem;
+
+		// Loop through all the elements in order (not including the starting
+		// element), trying to focus them, until we reach the place we started
+		// again, which means that no element wanted to be focused.
+		Elem *next = getNextElem(start, reverse);
+
+		while (next != start) {
+			if (requestFocusedElem(next, true)) {
+				return;
+			}
+
+			next = getNextElem(next, reverse);
+		}
+	}
+
+	void Window::updateFocusedElem()
+	{
+		SDL_Event notice = createUiEvent(UI_FOCUS_REQUEST);
+
+		// Ask all elements that the mouse just clicked on if they want to
+		// be the focused element.
+		Elem *new_focused = sendHoveredInput(notice);
+
+		// If an element responded to the request that is different from the
+		// currently focused element, then update the focused element.
+		if (new_focused != nullptr && m_focused_elem != new_focused) {
+			changeFocusedElem(new_focused, true);
+		}
+	}
+
+	void Window::updateHoveredElem()
+	{
+		SDL_Event notice = createUiEvent(UI_HOVER_REQUEST);
+
+		// If the window is focused, ask all elements that the mouse is
+		// currently inside if they want to be the hovered element. Otherwise,
+		// make no element hovered.
+		Elem *old_hovered = m_hovered_elem;
+		Elem *new_hovered = nullptr;
+
+		if (isFocused()) {
+			new_hovered = sendHoveredInput(notice);
+		}
+
+		// If a different element responded to the hover request (or no element
+		// at all), then update the hovered element.
+		if (old_hovered != new_hovered) {
+			m_hovered_elem = new_hovered;
+
+			// Let the old and new hovered elements know that things have
+			// changed, and their parent elements too.
+			notice = createUiEvent(UI_HOVER_CHANGED, old_hovered, new_hovered);
+
+			sendTreeInput(old_hovered, notice, false);
+			sendTreeInput(new_hovered, notice, false);
+		}
+	}
+
+	bool Window::isPointerOutside() const
+	{
+		// If the mouse is inside any element, it's not outside the window. We
+		// have to check every element, not just the root, because elements may
+		// have the noclip property set.
+		for (Elem *elem : m_ordered_elems) {
+			if (elem->hasPointedBox()) {
+				return false;
+			}
+		}
+
+		return true;
 	}
 
 	void Window::readElems(std::istream &is,
@@ -252,7 +638,8 @@ namespace ui
 		}
 	}
 
-	bool Window::updateElems(std::unordered_map<Elem *, std::string> &elem_contents)
+	bool Window::updateElems(std::unordered_map<Elem *, std::string> &elem_contents,
+			bool set_focus, Elem *new_focused)
 	{
 		// Now that we have a fully updated window, we can update each element
 		// with its contents and set up the parent-child relations. We couldn't
@@ -275,6 +662,19 @@ namespace ui
 		if (m_elems.size() != m_ordered_elems.size()) {
 			errorstream << "Window " << m_id << " has orphaned elements" << std::endl;
 			return false;
+		}
+
+		// If the user wants to focus a new element or unfocus the current
+		// element, remove focus from the current element and request focus on
+		// the new element.
+		if (set_focus && new_focused != m_focused_elem) {
+			if (new_focused != nullptr) {
+				if (!requestFocusedElem(new_focused, false)) {
+					changeFocusedElem(nullptr, false);
+				}
+			} else {
+				changeFocusedElem(nullptr, false);
+			}
 		}
 		return true;
 	}

omicron1100/luanti

src/ui/window.cpp
@@ -15,6 +15,8 @@
 #include "ui/static_elems.h"
 #include "util/serialize.h"
 
+#include <SDL2/SDL.h>
+
 namespace ui
 {
 	SizeI getTextureSize(video::ITexture *texture)
@@ -51,6 +53,30 @@ namespace ui
 		return nullptr;
 	}
 
+	Elem *Window::getNextElem(Elem *elem, bool reverse)
+	{
+		size_t next = elem->getOrder();
+		size_t last = m_ordered_elems.size() - 1;
+
+		if (!reverse) {
+			next = (next == last) ? 0 : next + 1;
+		} else {
+			next = (next == 0) ? last : next - 1;
+		}
+
+		return m_ordered_elems[next];
+	}
+
+	void Window::clearElem(Elem *elem)
+	{
+		if (m_focused_elem == elem) {
+			m_focused_elem = nullptr;
+		}
+		if (m_hovered_elem == elem) {
+			m_hovered_elem = nullptr;
+		}
+	}
+
 	const std::string *Window::getStyleStr(u32 index) const
 	{
 		if (index < m_style_strs.size()) {
@@ -67,10 +93,18 @@ namespace ui
 		m_root_elem = nullptr;
 
 		m_style_strs.clear();
+
+		m_focused_elem = nullptr;
+		m_hovered_elem = nullptr;
+
+		m_uncloseable = false;
+		m_events = 0;
 	}
 
 	bool Window::read(std::istream &is, bool opening)
 	{
+		// Read in all the fundamental properties that must be unconditionally
+		// provided for the window.
 		std::unordered_map<Elem *, std::string> elem_contents;
 		readElems(is, elem_contents);
 
@@ -83,8 +117,26 @@ namespace ui
 		if (opening)
 			m_type = toWindowType(readU8(is));
 
-		// Finally, we can proceed to read in all the element properties.
-		return updateElems(elem_contents);
+		// After the unconditional properties, read the conditional ones.
+		u32 set_mask = readU32(is);
+
+		bool set_focus = false;
+		Elem *new_focused = nullptr;
+
+		if (testShift(set_mask)) {
+			new_focused = getElem(readNullStr(is), false);
+			set_focus = true;
+		}
+
+		if (opening)
+			m_uncloseable = testShift(set_mask);
+
+		if (testShift(set_mask))
+			enableEvent(ON_SUBMIT);
+		if (testShift(set_mask))
+			enableEvent(ON_FOCUS_CHANGE);
+
+		return updateElems(elem_contents, set_focus, new_focused);
 	}
 
 	float Window::getScale() const
@@ -96,6 +148,14 @@ namespace ui
 	{
 		SizeF size = RenderingEngine::get_video_driver()->getCurrentRenderTargetSize();
 		return size / getScale();
+	}
+
+	PosF Window::getPointerPos() const
+	{
+		int x, y;
+		SDL_GetMouseState(&x, &y);
+
+		return PosF(x, y) / getScale();
 	}
 
 	void Window::drawRect(RectF dst, RectF clip, video::SColor color)
@@ -147,8 +207,334 @@ namespace ui
 		RectF layout_rect(getScreenSize());
 		backdrop.relayout(layout_rect, layout_rect);
 
+		// Find the current hovered element, which will be nothing if we're not
+		// focused. This may have changed between draw calls due to window
+		// resizes or element layouting.
+		hoverPointedElem();
+
+		// If this window isn't focused, tell the currently focused element.
+		if (!isFocused()) {
+			SDL_Event notice = createUiEvent(UI_FOCUS_SUBVERTED);
+			sendTreeInput(m_focused_elem, notice, true);
+		}
+
 		// Draw all of the newly layouted and updated elements.
 		backdrop.draw();
+	}
+
+	bool Window::isFocused() const
+	{
+		return g_manager.isFocused() && g_manager.getFocused() == this;
+	}
+
+	bool Window::processInput(const SDL_Event &event)
+	{
+		switch (event.type) {
+		case SDL_KEYDOWN:
+		case SDL_KEYUP: {
+			// Send the keypresses off to the focused element for processing.
+			// The hovered element never gets keypresses.
+			if (sendFocusedInput(event) != nullptr) {
+				return true;
+			}
+
+			if (event.type == SDL_KEYDOWN) {
+				u16 mod = event.key.keysym.mod;
+
+				switch (event.key.keysym.sym) {
+				case SDLK_ESCAPE:
+					// If we got an escape keypress, close the window.
+					destroyWindow();
+					return true;
+
+				case SDLK_RETURN:
+				case SDLK_RETURN2:
+				case SDLK_KP_ENTER:
+					// If the enter key was pressed but not handled by any
+					// elements, send a submit event to the server.
+					if (testEvent(ON_SUBMIT)) {
+						g_manager.sendMessage(createEvent(ON_SUBMIT).str());
+					}
+					return true;
+
+				case SDLK_TAB:
+				case SDLK_KP_TAB:
+					// If we got a tab key press, but not a ctrl-tab (which is
+					// reserved for use by elements), focus the next element,
+					// or the previous element if shift is pressed.
+					if (!(mod & KMOD_CTRL)) {
+						focusNextElem(mod & KMOD_SHIFT);
+						return true;
+					}
+					break;
+
+				default:
+					break;
+				}
+			}
+
+			return false;
+		}
+
+		case SDL_MOUSEMOTION:
+		case SDL_MOUSEBUTTONDOWN:
+		case SDL_MOUSEBUTTONUP:
+		case SDL_MOUSEWHEEL: {
+			// Make sure that we have an updated hovered element so that the
+			// hovered element is the one that receives the mouse motion event.
+			if (event.type == SDL_MOUSEMOTION) {
+				hoverPointedElem();
+			}
+
+			// If we just clicked with the left mouse button, see if there's
+			// any element at that position to focus.
+			if (event.type == SDL_MOUSEBUTTONDOWN &&
+					event.button.button == SDL_BUTTON_LEFT) {
+				if (isPointerOutside()) {
+					changeFocusedElem(nullptr, true);
+				} else {
+					focusPointedElem();
+				}
+			}
+
+			// First, give the focused element a chance to see the mouse event,
+			// so it can e.g. unpress a button if a mouse button was released.
+			if (sendFocusedInput(event) != nullptr) {
+				return true;
+			}
+
+			// Then, send the mouse input to the hovered element.
+			if (sendPointedInput(event) != nullptr) {
+				return true;
+			}
+
+			// If we double clicked outside any element, then close the window.
+			if (event.type == SDL_MOUSEBUTTONDOWN &&
+					event.button.button == SDL_BUTTON_LEFT &&
+					event.button.clicks >= 2 && isPointerOutside()) {
+				destroyWindow();
+				return true;
+			}
+
+			return false;
+		}
+
+		default:
+			return false;
+		}
+	}
+
+	void Window::enableEvent(u32 event)
+	{
+		m_events |= (1 << event);
+	}
+
+	bool Window::testEvent(u32 event) const
+	{
+		return m_events & (1 << event);
+	}
+
+	std::ostringstream Window::createEvent(u32 event) const
+	{
+		auto os = newOs();
+
+		writeU8(os, Manager::WINDOW_EVENT);
+		writeU64(os, m_id);
+		writeU8(os, event);
+
+		return os;
+	}
+
+	void Window::destroyWindow()
+	{
+		if (!m_uncloseable) {
+			// Always send the close event so the server can update its
+			// internal window tables, even if there's no on_close() handler.
+			g_manager.sendMessage(createEvent(ON_CLOSE).str());
+
+			// This causes the window object to be destroyed. Do not run any
+			// code after this!
+			g_manager.removeWindow(m_id);
+		}
+	}
+
+	Elem *Window::sendTreeInput(Elem *elem, const SDL_Event &event, bool direct)
+	{
+		// Give the event to the element and all its parents for processing.
+		while (elem != nullptr) {
+			bool handled = elem->processInput(event);
+
+			if (handled) {
+				// If we handled the event, return the element that handled it.
+				return elem;
+			} else if (direct) {
+				// If this event is only intended directly for this element and
+				// it didn't handle the event, then we're done.
+				return nullptr;
+			}
+
+			// Otherwise, give the parent a chance to handle it.
+			elem = elem->getParent();
+		}
+		return nullptr;
+	}
+
+	Elem *Window::sendPointedInput(const SDL_Event &event)
+	{
+		// We want to get the topmost hovered element, so we have to iterate in
+		// reverse draw order and check each element the mouse is inside.
+		for (size_t i = m_ordered_elems.size(); i > 0; i--) {
+			Elem *elem = m_ordered_elems[i - 1];
+			if (elem->getMain().isContentPointed() && elem->processInput(event)) {
+				return elem;
+			}
+		}
+
+		return nullptr;
+	}
+
+	Elem *Window::sendFocusedInput(const SDL_Event &event)
+	{
+		if (m_focused_elem == nullptr) {
+			return nullptr;
+		}
+
+		// Send the event to the focused element and its parents.
+		Elem *handled = sendTreeInput(m_focused_elem, event, false);
+
+		// If one of the focused element's parents handled the event, let the
+		// focused element know that focus was subverted.
+		if (handled != nullptr && handled != m_focused_elem) {
+			SDL_Event notice = createUiEvent(UI_FOCUS_SUBVERTED);
+			sendTreeInput(m_focused_elem, notice, true);
+		}
+
+		return handled;
+	}
+
+	void Window::changeFocusedElem(Elem *new_focused, bool send_event)
+	{
+		// If the same element is being focused, do nothing.
+		if (new_focused == m_focused_elem) {
+			return;
+		}
+
+		Elem *old_focused = m_focused_elem;
+		m_focused_elem = new_focused;
+
+		// Let the old and new focused elements know that things have
+		// changed, and their parent elements too.
+		SDL_Event notice = createUiEvent(UI_FOCUS_CHANGED, old_focused, new_focused);
+
+		sendTreeInput(old_focused, notice, false);
+		sendTreeInput(new_focused, notice, false);
+
+		// If the server wants to know when focus changes, send it an event.
+		if (send_event && testEvent(ON_FOCUS_CHANGE)) {
+			auto os = createEvent(ON_FOCUS_CHANGE);
+
+			// If either the old or the new element was unfocused, send an
+			// empty string. Otherwise, send the ID of the element.
+			writeNullStr(os, old_focused == nullptr ? "" : old_focused->getId());
+			writeNullStr(os, new_focused == nullptr ? "" : new_focused->getId());
+
+			g_manager.sendMessage(os.str());
+		}
+	}
+
+	bool Window::requestFocusedElem(Elem *new_focused, bool send_event)
+	{
+		// If this element is already focused, we don't need to do anything.
+		if (new_focused == m_focused_elem) {
+			return m_focused_elem;
+		}
+
+		SDL_Event notice = createUiEvent(UI_FOCUS_REQUEST);
+
+		// Ask the new element if it can take user focus. If it can, make it
+		// the focused element.
+		if (sendTreeInput(new_focused, notice, true) == new_focused) {
+			changeFocusedElem(new_focused, send_event);
+			return true;
+		}
+		return false;
+	}
+
+	void Window::focusNextElem(bool reverse)
+	{
+		// Start tabbing from the focused element if there is one, or the root
+		// element otherwise.
+		Elem *start = m_focused_elem != nullptr ? m_focused_elem : m_root_elem;
+
+		// Loop through all the elements in order (not including the starting
+		// element), trying to focus them, until we reach the place we started
+		// again, which means that no element wanted to be focused.
+		Elem *next = getNextElem(start, reverse);
+
+		while (next != start) {
+			if (requestFocusedElem(next, true)) {
+				return;
+			}
+
+			next = getNextElem(next, reverse);
+		}
+	}
+
+	void Window::focusPointedElem()
+	{
+		SDL_Event notice = createUiEvent(UI_FOCUS_REQUEST);
+
+		// Ask all elements that the mouse just clicked on if they want to
+		// be the focused element.
+		Elem *new_focused = sendPointedInput(notice);
+
+		// If an element responded to the request that is different from the
+		// currently focused element, then update the focused element.
+		if (new_focused != nullptr && m_focused_elem != new_focused) {
+			changeFocusedElem(new_focused, true);
+		}
+	}
+
+	void Window::hoverPointedElem()
+	{
+		SDL_Event notice = createUiEvent(UI_HOVER_REQUEST);
+
+		// If the window is focused, ask all elements that the mouse is
+		// currently inside if they want to be the hovered element. Otherwise,
+		// make no element hovered.
+		Elem *old_hovered = m_hovered_elem;
+		Elem *new_hovered = nullptr;
+
+		if (isFocused()) {
+			new_hovered = sendPointedInput(notice);
+		}
+
+		// If a different element responded to the hover request (or no element
+		// at all), then update the hovered element.
+		if (old_hovered != new_hovered) {
+			m_hovered_elem = new_hovered;
+
+			// Let the old and new hovered elements know that things have
+			// changed, and their parent elements too.
+			notice = createUiEvent(UI_HOVER_CHANGED, old_hovered, new_hovered);
+
+			sendTreeInput(old_hovered, notice, false);
+			sendTreeInput(new_hovered, notice, false);
+		}
+	}
+
+	bool Window::isPointerOutside() const
+	{
+		// If the mouse is inside any element, it's not outside the window. We
+		// have to check every element, not just the root, because elements may
+		// have the noclip property set. However, the backdrop is not included.
+		for (Elem *elem : m_ordered_elems) {
+			if (elem->getMain().isContentPointed()) {
+				return false;
+			}
+		}
+
+		return true;
 	}
 
 	void Window::readElems(std::istream &is,
@@ -258,7 +644,8 @@ namespace ui
 		}
 	}
 
-	bool Window::updateElems(std::unordered_map<Elem *, std::string> &elem_contents)
+	bool Window::updateElems(std::unordered_map<Elem *, std::string> &elem_contents,
+			bool set_focus, Elem *new_focused)
 	{
 		// Now that we have a fully updated window, we can update each element
 		// with its contents and set up the parent-child relations. We couldn't
@@ -281,6 +668,19 @@ namespace ui
 		if (m_elems.size() != m_ordered_elems.size()) {
 			errorstream << "Window " << m_id << " has orphaned elements" << std::endl;
 			return false;
+		}
+
+		// If the user wants to focus a new element or unfocus the current
+		// element, remove focus from the current element and request focus on
+		// the new element.
+		if (set_focus && new_focused != m_focused_elem) {
+			if (new_focused != nullptr) {
+				if (!requestFocusedElem(new_focused, false)) {
+					changeFocusedElem(nullptr, false);
+				}
+			} else {
+				changeFocusedElem(nullptr, false);
+			}
 		}
 		return true;
 	}